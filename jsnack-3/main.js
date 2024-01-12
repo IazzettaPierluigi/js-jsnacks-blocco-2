774bfab18670060d37e8bfd97940d091ac7e5966
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-01-12 17:32:19 +0100

    jsnack3 done with FOR

--- jsnack-3/main.js
+++ jsnack-3/main.js
@@ -1,21 +1,18 @@
 // Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
 
 let primoArray = [1, 2, 3, 4, 5, 6];
-let secondoArray = [5, 6, 7];
+let secondoArray = [5, 6];
 
 
-while (primoArray.length !== secondoArray.length) {
-
-  // Aggiungi un elemento all'array più corto
+for (; primoArray.length !== secondoArray.length; ) {
+ 
   if (primoArray.length < secondoArray.length) {
-
-    primoArray.push(Math.floor(Math.random() * 100) + 1); 
-
+    primoArray.push(Math.floor(Math.random() * 100) + 1);
   } else {
-
-    secondoArray.push(Math.floor(Math.random() * 100) + 1); 
+    secondoArray.push(Math.floor(Math.random() * 100) + 1);
   }
 }
+
 
 // Stampare gli array finali
 console.log("Primo array:", primoArray);
